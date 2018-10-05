582891eb6eabc2fdb0f065950617255ac7618484
RogerSV9 <rogersanvi.sanchez@gmail.com>
2018-10-05 11:49:19 +0200

    Second commit

--- src/main/java/edu/upc/eetac/dsa/Singleton.java
+++ src/main/java/edu/upc/eetac/dsa/Singleton.java
@@ -1,13 +1,18 @@
 package edu.upc.eetac.dsa;
 
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
 public class Singleton {
     private static Singleton instance;
-    // [data]
+    private ResourceBundle ca, es, en;
+    HashMap<String,Singleton> hashMap = new HashMap<String,Singleton>();
+    private static final String PACKAGE = "edu.upc.eetac.dsa";
     private Singleton() {
-        //data= new ..
+
     }
     public static Singleton getInstance(){
-        if (instance==null) instance=new Singleton();
+        if (instance==null){ instance=new Singleton();}
         return instance;
     }
 
